1e80a60d703306a58761e54f92078b01e0916eb0
Taiming Chen <eefozeo@gmail.com>
2018-08-22 11:55:32 +0800

    update to 107 course data

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const COURSE_DATA_DOMAIN='https://raw.githubusercontent.com/TimingJL/nccucourse-data/master';
+// export const COURSE_DATA_DOMAIN='https://raw.githubusercontent.com/TimingJL/nccucourse-data/master';
+export const COURSE_DATA_DOMAIN='https://raw.githubusercontent.com/progeneral777/nccu_course_data/master';
